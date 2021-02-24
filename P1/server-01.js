52225e310e5f645fdd7ceb4f196f153a073a8d32
mrojbar <miguel24rb@gmail.com>
2021-02-24 20:38:46 +0100

    mas pruebas node

--- P1/server-01.js
+++ P1/server-01.js
@@ -6,22 +6,22 @@ const PUERTO = 8080;
 let count = 0;
 //-- Crear el servidor
 const server = http.createServer((req, res) => {
-  //-- req: http.IncomingMessage: Mensaje de solicitud
-  //-- res: http.SercerResponse: Mensaje de respuesta (vacío)    
+    //-- req: http.IncomingMessage: Mensaje de solicitud
+    //-- res: http.SercerResponse: Mensaje de respuesta (vacío)
 
-  //-- Indicamos que se ha recibido una petición
-  count++;
-  console.log("Petición "+count+" recibida!");
+    //-- Indicamos que se ha recibido una petición
+    count++;
+    console.log("Petición "+count+" "+req.method+" recibida!");
 
-  //-- Cabecera que indica el tipo de datos del
-  //-- cuerpo de la respuesta: Texto plano
-  res.setHeader('Content-Type', 'text/plain');
+    //-- Cabecera que indica el tipo de datos del
+    //-- cuerpo de la respuesta: Texto plano
+    res.setHeader('Content-Type', 'text/plain');
 
-  //-- Mensaje del cuerpo
-  res.write("Soy el Happy server!!\n");
+    //-- Mensaje del cuerpo
+    res.write("Soy el Happy server!!\n");
 
-  //-- Terminar la respuesta y enviarla
-  res.end();
+    //-- Terminar la respuesta y enviarla
+    res.end();
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
