178acbc1a07c57f5fbe25a0ce32f14724476794f
mrojbar <miguel24rb@gmail.com>
2021-03-15 12:42:43 +0100

    Ejemplos json

---
+++ P2/EjTienda1.js
@@ -0,0 +1,24 @@
+//-- Crear la variable tienda, que es una estructura formada
+//-- por dos productos en json.
+const tienda_json = `[
+    {
+      "nombre": "Alhambra II",
+      "descripcion": "Placa con FPGA ice40HX8K",
+      "stock": 3
+    },
+    {
+      "nombre": "Icestick",
+      "stock": 10
+    }
+  ]`
+  
+  const tienda = JSON.parse(tienda_json); //parseamos el json en objetos js.
+
+  //-- Mostrar informacion sobre la tienda
+  console.log("Productos en la tienda: " + tienda.length);
+  
+  //-- Recorrer el array de productos
+  tienda.forEach((element, index)=>{
+    console.log("Producto: " + (index + 1) + ": " + element.nombre);
+  });
+  
