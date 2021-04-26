7953967a458eb61841e34c48e1f9ae2b30f7f5b4
mrojbar <miguel24rb@gmail.com>
2021-04-27 00:32:42 +0200

    P1 cambios parse

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
-const PUERTO = 8080
+const PUERTO = 9000;
 let count = 0;
 let CABECERAS = false;
 
@@ -12,10 +12,10 @@ http.createServer((req, res) => {
   console.log("");
   console.log("----------> Peticion recibida")
   console.log("======================");
-  console.log("Mensaje de solicitud "+ count);
+  console.log("Mensaje de solicitud " + count);
   console.log("Método: " + req.method);
   console.log("Version: " + req.httpVersion)
-  if (CABECERAS){
+  if (CABECERAS) {
     console.log("Cabeceras: ");
     for (hname in req.headers)
       console.log(`  * ${hname}: ${req.headers[hname]}`);
@@ -29,55 +29,57 @@ http.createServer((req, res) => {
 
   let filename = ""
   let recurso = ""
+  let tipostr = ""
   let mime = "text/"
 
   // -- Contar slash
   var nSlash = (q.pathname.match(new RegExp("/", "g")) || []).length;
-  var nSlashDoble = (q.href.match(new RegExp("//", "g")) || []).length;
+  //var nSlashDoble = (q.href.match(new RegExp("//", "g")) || []).length;
 
-  // -- Buscamos el "." final para poder indicar que tipo mime es
-  let tipo = q.pathname.lastIndexOf(".")
-  let tipostr = q.pathname.slice(tipo+1) 
-  console.log("Este es el tipo: "+tipostr);
+  // -- Obtener el tipo de archivo
+  if (q.pathname.includes('.')) {
+    tipostr = q.pathname.substring(q.pathname.lastIndexOf('.') + 1); //quedarse con lo que hay detras del punto
+  } else {
+    tipostr = ""; //tipo por defecto
+  }
+  console.log("Tipo: " + tipostr);
 
-  // -- If para completar el nombre del recurso y tipo mime del mismo
+  // -- Recurso y Mime ------------------- mejorar.
   if (nSlash > 1) {
-    console.log("Entro en FOR");
     recurso = "." + q.pathname
     mime = mime + tipostr
   } else {
-    if (tipostr == "/") {
+    if (tipostr == "" && q.pathname == "/") {
       mime = "text/html"
       filename += "index.html"
     } else {
       mime = mime + tipostr
     }
-    let peticion = q.pathname.lastIndexOf("/")
-    recurso = q.pathname.slice(peticion+1)
+    recurso = q.pathname.slice(q.pathname.lastIndexOf("/") + 1)
   }
 
-  filename = filename + recurso;
-  console.log("Slash contados: "+nSlash);
-  console.log("Este es el filename: "+filename);
-  console.log("Este es el mime: "+mime);
+  filename += recurso;
+  console.log("Slash contados: " + nSlash);
+  console.log("Filename: " + filename);
+  console.log("Mime: " + mime);
 
   //-- Leer fichero, data es el contenido del fichero leido
-  fs.readFile(filename, function(err, data) {
+  fs.readFile(filename, function (err, data) {
     //-- Fichero no encontrado. Devolver mensaje de error
     if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.writeHead(404, { 'Content-Type': 'text/html' });
       res.end("404 Not found");
     }
     else {
       //-- Generar el mensaje de respuesta
-      res.writeHead(200, {'Content-Type': mime});
+      res.writeHead(200, { 'Content-Type': mime });
       res.write(data);
       res.end();
     }
     console.log("");
     console.log("======================");
-    console.log("Mensaje de respuesta "+count);
-    console.log("Respuesta: "+res.statusCode);
+    console.log("Mensaje de respuesta " + count);
+    console.log("Respuesta: " + res.statusCode);
   });
 
 }).listen(PUERTO);
---
+++ TEST/test.js
@@ -0,0 +1,7 @@
+const colors = require('colors');
+
+console.log("Holiiiii".yellow);
+console.log("Esto es rojo".red)
+console.log("Y esto rojo resaltado...".red.bold);
+console.log(('En arco iris...').rainbow);
+console.log('Negro sobre blanco...!'.black.bgWhite);
