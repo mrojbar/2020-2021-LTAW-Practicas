daf2d22feaa0b9817d81a02e6d16ceb886426564
mrojbar <miguel24rb@gmail.com>
2021-05-13 01:11:20 +0200

    cookies implementadas

--- P2/tienda.js
+++ P2/tienda.js
@@ -9,10 +9,11 @@ const tienda_json = fs.readFileSync(FICHERO_JSON);
 const tienda = JSON.parse(tienda_json); // objeto tienda javascript
 
 // -- Objetos generados.
-const LOGIN = fs.readFileSync('login.html', 'utf-8');
 const ERROR404 = fs.readFileSync('404.html', 'utf-8');
+const LOGIN = fs.readFileSync('login.html', 'utf-8');
 const LOGINOK = fs.readFileSync('loginok.html', 'utf-8');
 const CHECKOUT = fs.readFileSync('checkout.html', 'utf-8');
+const CHECKOUTOK = fs.readFileSync('checkoutok.html', 'utf-8');
 
 //console.log(tienda[0]["orders"][0]["shoppinglist"][1]["name"]);
 //-- Configurar y lanzar el servidor.
@@ -43,6 +44,24 @@ http.createServer((req, res) => {
   let tipostr = ""
   let mime = ""
 
+  //-- Funcion leer cookies, devuelve la cookie pedida o null si no existe
+  function get_cookie(req, cookiename) {
+    let cookievalor = null;
+    const cookie = req.headers.cookie;
+    if (cookie) {
+      let pares = cookie.split(";");
+      //-- Recorrer todos los pares nombre-valor
+      pares.forEach((element) => {
+        //-- Obtener los nombres y valores por separado
+        let [nombre, valor] = element.split('=');
+        //-- Leer el nombre de la cookie especificada
+        if (nombre.trim() === cookiename) {
+          cookievalor = valor;
+        }
+      });
+      return cookievalor || null; //-- devolver null si no existe
+    }
+  }
 
   // -- Contar slash
   var nSlash = (recurso.match(new RegExp("/", "g")) || []).length;
@@ -86,7 +105,7 @@ http.createServer((req, res) => {
   if (tipostr == "" && recurso != "/") {
     let datagen = ""; // datos generados.
 
-    if (recurso == '/productos') {//devuelveJsonProductos(res); // caso de acceso a productos
+    if (recurso == '/productos') {// caso de acceso a productos
       mime = "application/json";
       datagen = JSON.stringify(tienda[1]["products"]);
     }
@@ -98,59 +117,73 @@ http.createServer((req, res) => {
 
     else if (recurso == '/checkout') {
       mime = "text/html"
-      datagen = CHECKOUT;
+      datagen = CHECKOUT.replace("HTML_EXTRA", "");
     }
 
     else if (recurso.startsWith("/procesarlogin")) {
       mime = "text/html"
       foundflag = false;
-      username = url.searchParams.get('nombre');
-      console.log("User: " + username);
-
+      let username = url.searchParams.get('nombre');
+      let realname = "";
       tienda[2]["users"].forEach((element, index) => {
-        if (element.username == username) foundflag = true; //////////////////continuar cookies aqui
+        if (element.username == username){
+          foundflag = true; //-- el usuario existe en la base de datos
+          realname = element.fullname;
+        }
       });
       if (foundflag) {
-        datagen = LOGINOK;
+        datagen = LOGINOK.replace("HTML_EXTRA", "<h3>LOGIN CORRECTO. Bienvenido/a de nuevo: " + realname + "</h3>");
+        res.setHeader('Set-Cookie', "username=" + username); //-- generar cookie de login
+        console.log("login de: " + get_cookie(req, "username"));
       }
       else datagen = LOGIN;
     }
 
+    else if (recurso.startsWith("/logout")){
+      res.setHeader('Set-Cookie', "username="); //-- borrar cookie de login
+      datagen = LOGINOK.replace("HTML_EXTRA", "<h3>SE HA CERRADO LA SESIÓN. HASTA PRONTO</h3>");;
+    }
+    
     else if (recurso.startsWith("/procesarcheckout")) {
-      mime = "text/html"
-      foundflag = false;
-      address = url.searchParams.get('address');
-      creditcard = url.searchParams.get('creditcard');
-      if (address == "" || creditcard == ""){
-        console.log("Hay datos vacios!");
-        datagen = CHECKOUT; // redirigir
-        return; // salir si datos son null.
-      }
-      console.log("sigue")
-      username = "elpatata" //-------- EJ COOKIES cambiar por usuario actual
-      //shoppinglist = // -------- EJ COOKIES cambiar por carrito actual
-      console.log("Direccion: " + address + " Tarjeta: " + creditcard);
+      mime = "text/html";
+      let foundflag = false;
+      let address = url.searchParams.get('address');
+      let creditcard = url.searchParams.get('creditcard');
+      let username = get_cookie(req, "username") //-- usuario actual
+      let shoppinglist = ""; // -------- EJ COOKIES cambiar por carrito actual
 
-      tienda[0]["orders"].forEach((element, index) => { // ----------- cambiar username por el actual cookies.
-        if (element.username == username) { // comprobar si ya hay un pedido y sustituir.
-          element.address = address;
-          element.creditcard = creditcard;
-          element.shoppinglist = "chungo"
-          foundflag = true;
-        }
-      });
-      if (!foundflag) {
-        let neworder = {
-          "user": username, // ---------- cambiar cookies
-          "dirección": address,
-          "tarjeta": creditcard,
-          "shoppinglist": "listadeproductosaqui" // ----------- cambiar cookies
-        }
-        tienda[0]["orders"].push(neworder); // meter nuevo pedido
+      if (address == "" || creditcard == "") { // datos vacios
+        console.log("DATOS VACIOS");
+        datagen = CHECKOUT.replace("HTML_EXTRA", "<h3>NO SE PUEDE PROCEDER: HAY DATOS VACÍOS</h3>");
       }
-      // -- escribir archivo json
-      console.log(JSON.stringify(tienda)); ////// -- debug
-      fs.writeFileSync("tiendaout.json", JSON.stringify(tienda));
+      else if (username == null) { // no ha inicado sesion
+        datagen = CHECKOUT.replace("HTML_EXTRA", "<h3>DEBE INICIAR SESION PRIMERO PARA PODER REALIZAR LA COMPRA</h3>");
+      }
+      else {
+        console.log("Direccion: " + address + " Tarjeta: " + creditcard);
+
+        tienda[0]["orders"].forEach((element, index) => { // ----------- cambiar username por el actual cookies.
+          if (element.username == username) { // comprobar si ya hay un pedido y sustituir.
+            element.address = address;
+            element.creditcard = creditcard;
+            element.shoppinglist = shoppinglist; // ---------- cambiar cookies
+            foundflag = true;
+          }
+        });
+        if (!foundflag) {
+          let neworder = {
+            "username": username, // ---------- cambiar cookies
+            "address": address,
+            "creditcard": creditcard,
+            "shoppinglist": shoppinglist // ----------- cambiar cookies
+          }
+          tienda[0]["orders"].push(neworder); // meter nuevo pedido
+        }
+        // -- escribir archivo json
+        console.log(JSON.stringify(tienda[0]["orders"])); ////// -- debug
+        fs.writeFileSync("tiendaout.json", JSON.stringify(tienda));
+        datagen = CHECKOUTOK;
+      }
     }
 
     else { // -- Si el recurso no existe
